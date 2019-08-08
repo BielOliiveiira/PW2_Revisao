727fbb1a15d9e6ecdbe727981e92abe51bd2b53f
BielOliiveiira <gabriel_vm@live.com>
2019-08-07 22:13:42 -0300

    Teste do IF

---
+++ dist/exercicio.js
@@ -0,0 +1,12 @@
+var nota1 = 10;
+var nota2 = 9;
+var nota3 = 5;
+var media;
+media = (nota1 + nota2 + nota3) / 3;
+console.log("A média das notas é " + media);
+if (media >= 7) {
+    console.log("Aprovado");
+}
+else {
+    console.log("Reprovado");
+}
---
+++ exercicio.ts
@@ -0,0 +1,9 @@
+let nota1: number = 10
+let nota2: number = 9
+let nota3: number = 5
+let media: number 
+media = (nota1 + nota2 + nota3) / 3
+console.log("A média das notas é " + media)
+if (media >= 7)
+{console.log ("Aprovado")}else
+{console.log ("Reprovado")}
