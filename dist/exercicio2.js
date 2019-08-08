ec073717d4fb3ef55bb8a8f7302cf82fb31745b6
BielOliiveiira <gabriel_vm@live.com>
2019-08-07 22:30:05 -0300

    Doação de Sangue

---
+++ dist/exercicio2.js
@@ -0,0 +1,7 @@
+var idade = 88;
+if (idade >= 17 && idade <= 67) {
+    console.log("Doadora");
+}
+else {
+    console.log("Não Doadora");
+}
---
+++ exercicio2.ts
@@ -0,0 +1,9 @@
+let idade: number = 88
+if (idade >=17 && idade <=67)
+{
+    console.log("Doadora")
+}
+else
+{
+    console.log("Não Doadora")
+}
